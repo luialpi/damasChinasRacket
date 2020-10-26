b492cb30c3aad6aad444c1d0d1181ced46b3b729
Luis Alpizar Morales <luialpi@users.noreply.github.com>
2020-10-26 13:45:42 -0600

    Update ui.rkt

--- ui.rkt
+++ ui.rkt
@@ -1,89 +1,87 @@
 #lang racket
 
 ; —————————————————————————————————
-; seccion de imports y variables
+; section de imports y variables
 ; —————————————————————————————————
 
 (require graphics/graphics)
 (require htdp/matrix)
 (open-graphics)
 
-;;tamanios de la ventana
+;;window
 (define alto 1000)
 (define ancho 1200)
-
-;;Ventana para mostrar el tablero
 (define ventana1 (open-viewport "Damas Chinas" alto ancho))
 
-;;se utiliza para saber el tamanio de la matriz, expecificamente para el ui
+;;we use it to know the len of the matrix
 (define len '(0 1 2 3 4 5 6 7 8 9))
 
-;tablero de juego
-(define tablero (build-matrix 10 10 (lambda (x y) (* x 0))))
+;game board
+(define board (build-matrix 10 10 (lambda (x y) (* x 0))))
 
 
 ; —————————————————————————————————
-; seccion de logica de juego
+; game logic sention
 ; —————————————————————————————————
-;revisa si hay una ficha en el lugar i j
-(define (check-posicion i j) (= (matrix-ref tablero i j) 0))
+;check if we have a chip on i j
+(define (check-posicion i j) (= (matrix-ref board i j) 0))
 
-;;Pone una ficha en la posicion deseada del tablero
-;;Verifica si ya hay una ficha en el lugar
+;;puts a chip on the position i j 
+;;checks if we have a chip on that place
 (define (set-posicion i j ficha)
   (cond
-    [(check-posicion i j) (set! tablero (matrix-set tablero i j ficha))]
+    [(check-posicion i j) (set! board (matrix-set board i j ficha))]
     [else (print "No se puede mover, ya hay una ficha ahi.")]
  ))
 
-;;verifica si la diferencia en distancia entre dos movimientos es valida
-(define (check-distancia origen destino)
-  (define x (abs (- (list-ref origen 0) (list-ref destino 0))))
-  (define y (abs (- (list-ref origen 1) (list-ref destino 1))))
+;;checks if the distance between two movements is valid
+(define (check-distance origin destination)
+  (define x (abs (- (list-ref origin 0) (list-ref destination 0))))
+  (define y (abs (- (list-ref origin 1) (list-ref destination 1))))
   (or (= x 2) (= x 1) (= y 2) (= y 1))
-
 )
 
-;;verifica si un movimiento es valido
-;retorna true en caso de que el movimiento sea valido
-(define (check-movimiento origen destino)
+;;validates if movement is correct
+;returns true or false
+(define (check-movement origin destination)
   (cond
-    [(not (and (list? origen) (list? destino))) false ]
-    [ (or (check-posicion (list-ref origen 0) (list-ref origen 1)) (not (check-posicion (list-ref destino 0) (list-ref destino 1)))) false]
+    []
+    [(not (and (list? origin) (list? destination))) false ]
+    [ (or (check-posicion (list-ref origin 0) (list-ref origin 1)) (not (check-posicion (list-ref destination 0) (list-ref destination 1)))) false]
     [else
-     ;;verificamos los saltos
+     ;;check the jumps
      (cond
-       ;;verifico la posicion destino(1) +1
-       [(and (> (list-ref origen 1) (list-ref destino 1)) (check-distancia origen  destino))
-        (not (check-posicion (list-ref destino 0) (+ 1 (list-ref destino 1)))) ]
+       ;;check position with destination(1) +1
+       [(and (> (list-ref origin 1) (list-ref destination 1)) (check-distance origin  destination))
+        (not (check-posicion (list-ref destination 0) (+ 1 (list-ref destination 1)))) ]
 
-       ;;verifico la posicion destino(0) +1
-       [(and (> (list-ref origen 0) (list-ref destino 0)) (check-distancia origen  destino))
-        (not (check-posicion (+ 1 (list-ref destino 0)) (list-ref destino 1)))]
+       ;;check position with destination(0) +1
+       [(and (> (list-ref origin 0) (list-ref destination 0)) (check-distance origin  destination))
+        (not (check-posicion (+ 1 (list-ref destination 0)) (list-ref destination 1)))]
 
-       ;;verifico la posicion origen(1) +1
-       [(and (< (list-ref origen 1) (list-ref destino 1)) (check-distancia destino origen))
-        (not (check-posicion (list-ref origen 0) (+ 1 (list-ref origen 1))))]
+       ;;check position with origin(1) +1
+       [(and (< (list-ref origin 1) (list-ref destination 1)) (check-distance destination origin))
+        (not (check-posicion (list-ref origin 0) (+ 1 (list-ref origin 1))))]
 
-       ;;verifico la posicion origen(1) +1
-       [(and (< (list-ref origen 0) (list-ref destino 0)) (check-distancia destino  origen))
-        (not (check-posicion (+ 1 (list-ref destino 0)) (list-ref destino 1)))]
+       ;;check position with origin(1) +1
+       [(and (< (list-ref origin 0) (list-ref destination 0)) (check-distance destination  origin))
+        (not (check-posicion (+ 1 (list-ref destination 0)) (list-ref destination 1)))]
 
-       ;else, si no hay saltos, verificamos que el destino sea posible mover
-       [else (and (check-posicion (list-ref destino 0) (list-ref destino 1)) (check-distancia origen destino)) ]
+       ;else, if we dont have a jump then its a single movement so we procced to check it
+       [else (and (check-posicion (list-ref destination 0) (list-ref destination 1)) (check-distance origin destination)) ]
        )
      ]
     )
   )
 
-;;recorre la lista de movimientos verificando si es valido '( '(i j) '(i j) '(i j))
-;;esta se utiliza para cada ronda de movidas
-(define (check-movimientos lista)
+;;runs a list checking if each movement is valid, example of list: '( '(i j) '(i j) '(i j))
+;;we used to check the movements of the player, cause we are already validating the movements of the ia
+(define (check-movements list)
   (cond
-    [(or (empty? lista) (<= (length lista ) 1)) true]
+    [(or (empty? list) (<= (length list ) 1)) true]
     [else
      ( cond
-        [ (check-movimiento (first lista) (second lista)) (check-movimientos (cdr lista))]
+        [ (check-movement (first list) (second list)) (check-movements (cdr list))]
         [else false]
       )]
     )
@@ -92,40 +90,37 @@
 
 
 ; —————————————————————————————————
-; seccion de interfaz grafica
+; graphic interface section
 ; —————————————————————————————————
 
-;retorna la posicion (i, j) en el ui para dibujar el circulo deseado
+;returns the place in the board to draw a circle for each ij in the matrix
 (define (get-ui-pos i j)
   (list (+ 480 (- (* 50 j) (* 50 i))) (+ 25 (+ (* 50 j) (* 50 i)))))
 
-;dibuja ficha de color y posicion deseada
+;draw a solid cirlce of color
 (define (mueve-ficha color i j) (
-      (draw-solid-ellipse ventana1) (make-posn (list-ref (get-ui-pos i j) 0) (list-ref (get-ui-pos i j) 1) ) 50 50 color) )
+  (draw-solid-ellipse ventana1) (make-posn (list-ref (get-ui-pos i j) 0) (list-ref (get-ui-pos i j) 1) ) 50 50 color))
 
-;; define los valores iniciales
-
+;; initialice function, set up all the circles in the interface and also setup the initial positions in the matrix
 (define iniciar-matriz
   (for ([i len] )
     ( for ([j len])
        (cond
          [(or (and (= i 0) (<= j 3)) (and (= i 1) (<= j 2)) (and (= i 2) (<= j 1)) (and (= i 3) (= j 0)))
-          ;pone la ficha en el luhar del tablero
           (set-posicion i j 1)]
          [ (or (and (= i 9) (>= j 6)) (and (= i 8) (>= j 7)) (and (= i 7) (>= j 8)) (and (= i 6) (= j 9)))
-          ;pone la ficha en el luhar del tablero
           (set-posicion i j 2)]
      ))
 ))
 
-;dibuja los circulos grises del tablero inicial
+;draw all the circles 
 (define dibujar-circulos
   (for ([i len] )
     ( for ([j len])
        (cond
-         [(= (matrix-ref tablero i j) 1)
+         [(= (matrix-ref board i j) 1)
           (mueve-ficha "blue" i j)]
-         [ (= (matrix-ref tablero i j) 2)
+         [ (= (matrix-ref board i j) 2)
           (mueve-ficha "red" i j)]
          [else
           (mueve-ficha "DimGray" i j)
@@ -133,10 +128,28 @@
      ))
 ))
 
-;;dibuja los numeros para mejor ubicacion a nivel de ui
-(define dibujar-numeros
+;;draw the numbers of the ui so the user is able to see the position of the chip
+(define draw-numeros
   (for ([i len])
     ((draw-string ventana1) (make-posn (list-ref (get-ui-pos i 0) 0) (list-ref (get-ui-pos i 0) 1)) (number->string i)) 
     ((draw-string ventana1) (make-posn (+ 45 (list-ref (get-ui-pos 0 i) 0)) (- (list-ref (get-ui-pos 0 i) 1) 2)) (number->string i))
     )
 )
+
+(define (split-every-two list)
+   (if (not (empty? list))
+       (cons (take list 2) (split-every-two (drop list 2)))
+       '() ))
+
+(define (to-int list)
+  (filter-map (lambda (x) (string->number x)) list))
+
+(let loop ()
+  (display "Input: ")
+  (define a (string-split (read-line (current-input-port) 'any)))
+  (if (check-movements (split-every-two (to-int a)))
+      ()
+      (print "movement invalid")
+  )
+  
+  (loop))
